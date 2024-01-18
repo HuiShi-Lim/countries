d7f2381a7998f1097ad407b03ca9e00494bf8c7f
Ashley <huishi.lim@atoz-software.tech>
2024-01-18 09:10:00 +0800

    update detail page

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { MdKeyboardBackspace } from "react-icons/md";
 
-function CountryDetails({ countries: country, setCountries }) {
+function CountryDetails() {
   const { name } = useParams();
-//   const [testCountry, setTestCountry] = useState({});
-  console.log(country);
+  const [testCountry, setTestCountry] = useState({});
+  console.log(testCountry);
+
+  const printAllLanguages = (languages) => {
+    if (!languages) return;
+    return Object.values(languages);
+  };
+
+  const printAllCurrencies = (currency) => {
+    if (!currency) return;
+    return Object.values(currency).map((curr) => curr.name);
+  };
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/name/" + name.toLowerCase())
@@ -16,35 +27,74 @@ function CountryDetails({ countries: country, setCountries }) {
         return res.json();
       })
       .then((data) => {
-        setCountries(data);
+        setTestCountry(data[0]);
       });
   }, [name]);
 
   return (
-    <>
-      <Link to="/">Back</Link>
+    <div>
+      <Link to="/" className="py-1 px-20 flex items-center">
+        <MdKeyboardBackspace /> Back
+      </Link>
       <div>
-        CountryDetails
-        {Object.keys(country).length > 0 && (
-          <article>
+        {Object.keys(testCountry).length > 0 && (
+          <div className="flex justify-center">
             <img
-              src={country?.flags}
-              alt={country?.name}
+              src={testCountry.flags.svg}
+              alt={testCountry.name.common}
               className="rounded-t h-48 w-80 size-full"
             ></img>
-            {/* <div className="pl-6">
-              <p className="text-lg font-extrabold pt-5">{country?.name}</p>
-              <div className="pt-4 pb-11">
-                <p>Native Name: {country?.nativeName}</p>
-                <p>Population: {country.population}</p>
-                <p>Region: {testCountry.region}</p>
-                <p>Capital: {testCountry.capital}</p>
+            <div className="pl-20">
+              <p className="text-lg font-extrabold pt-5">
+                {testCountry.name.common}
+              </p>
+              <div className="pt-4 pb-11 grid grid-cols-2 gap-2">
+                <p>
+                  <b>Native Name:</b>
+                  {
+                    testCountry.name.nativeName[
+                      Object.keys(testCountry.name.nativeName)[2]
+                    ]?.common
+                  }
+                </p>
+                <p>
+                  <b>Population:</b> {testCountry.population}
+                </p>
+                <p>
+                  <b>Region:</b> {testCountry.region}
+                </p>
+                <p>
+                  <b>Sub Region:</b> {testCountry.subregion}
+                </p>
+                <p>
+                  <b>Capital:</b> {testCountry.capital}
+                </p>
+                <div>
+                  <p>
+                    <b>Top Level Domain:</b> {testCountry.tld}
+                  </p>
+                  {/* <p>Currencies: {testCountry.currencies[Object.keys(testCountry.currencies)[0]].name}</p>
+                <p>Languages: {testCountry.languages[Object.keys(testCountry.languages)[0]]}</p> */}
+
+                  <p>
+                    <b>Currencies:</b>{" "}
+                    {printAllCurrencies(testCountry.currencies)}
+                  </p>
+                  <p>
+                    <b>Languages:</b> {printAllLanguages(testCountry.languages)}
+                  </p>
+                </div>
               </div>
-            </div> */}
-          </article>
+              <div>
+                <p>
+                  <b>Border Countries</b> {testCountry.borders}
+                </p>
+              </div>
+            </div>
+          </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
